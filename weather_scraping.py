60bc42cb3ef044f266743d487ee6907b5beed2c1
Masa512411 <masa1717@gol.com>
2018-09-27 00:04:49 +0900

    first commit

---
+++ weather_scraping.py
@@ -0,0 +1,55 @@
+import json
+import os 
+import datetime
+
+import requests as req
+from bs4 import BeautifulSoup
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+@app.route("/webhook",methods=["POST"])
+def main():
+    req = request.get_json()
+    date = req["queryResult"]["parameters"]["data"]
+    weather = req["queryResult"]["parameters"]["weather"]
+    url = "https://tenki.jp/forecast/7/37/6710/34100/"
+
+    date = date.split("T")[0]
+    
+    today = datatime.datatime.today()
+    tomorrow = today + datetime.timedelta(days=1)
+
+
+    response = req.get(url)
+    soup = BeautifulSoup(response.text,"lxml")
+
+    if date == datetime.strftime(today,"%Y-%m-%d"):
+       today = soup.find(calss_="today-weather")
+       forecast_weather = today.p.string
+       speech = "今日の天気は{}です".format(forecast_weather)
+    elif data == datetime.strftime(tomorrow,"%Y-%m-%d"):
+        tomorrow = soup.find(class_="tomorrow-weather")
+        forcast_weather = tomorrow.p.string
+        speech = "明日の天気は{}です".format(forecast_weather)
+
+    res = make_response(jsonify({'speech':speech,'displayText':speech}))
+    res.headers['Content-type'] = 'application/json'
+    return res
+
+
+if __name__ == "__main__":
+    #port = int(os.environ.get("PORT",5000))
+    
+    #app.run(debug=False,port=port,host="0.0.0.0")
+    app.run(host="0.0.0.0",port = 3000)
+
+
+
+       
+
+    
