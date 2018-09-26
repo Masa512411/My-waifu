39bf3d06cef448c043b402892a0e36d069e1d788
Masa512411 <masa1717@gol.com>
2018-09-27 08:00:56 +0900

    good

--- weather_scraping.py
+++ weather_scraping.py
@@ -15,27 +15,28 @@ app = Flask(__name__)
 @app.route("/webhook",methods=["POST"])
 def main():
     req = request.get_json()
-    date = req["queryResult"]["parameters"]["data"]
+    date = req["queryResult"]["parameters"]["date"]
     weather = req["queryResult"]["parameters"]["weather"]
     url = "https://tenki.jp/forecast/7/37/6710/34100/"
 
     date = date.split("T")[0]
     
-    today = datatime.datatime.today()
+    today = datetime.datetime.today()
     tomorrow = today + datetime.timedelta(days=1)
 
 
     response = req.get(url)
+    print(response.text)
     soup = BeautifulSoup(response.text,"lxml")
 
     if date == datetime.strftime(today,"%Y-%m-%d"):
        today = soup.find(calss_="today-weather")
        forecast_weather = today.p.string
-       speech = "今日の天気は{}です".format(forecast_weather)
+       speech = "今日の広島の天気は{}です".format(forecast_weather)
     elif data == datetime.strftime(tomorrow,"%Y-%m-%d"):
         tomorrow = soup.find(class_="tomorrow-weather")
         forcast_weather = tomorrow.p.string
-        speech = "明日の天気は{}です".format(forecast_weather)
+        speech = "明日の広島の天気は{}です".format(forecast_weather)
 
     res = make_response(jsonify({'speech':speech,'displayText':speech}))
     res.headers['Content-type'] = 'application/json'
