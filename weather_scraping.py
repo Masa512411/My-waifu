fcdc9ca5b064bb34ca11a1207d4b0232553cb8b5
Masa512411 <masa1717@gol.com>
2018-09-27 10:35:25 +0900

    [fix]weather_scraping.py

--- weather_scraping.py
+++ weather_scraping.py
@@ -44,10 +44,9 @@ def main():
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",5000))
     
-    #app.run(debug=False,port=port,host="0.0.0.0")
-    app.run(host="0.0.0.0",port = 3000)
+    app.run(debug=False,port=port,host="0.0.0.0")
 
 
 
