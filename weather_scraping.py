4a3ae3a6ff5eba76aef21a35c9f611431324f8df
Masa512411 <masa1717@gol.com>
2018-09-27 16:43:48 +0900

    [fix]weather_scraping.py

--- weather_scraping.py
+++ weather_scraping.py
@@ -12,11 +12,11 @@ from flask import make_response
 
 app = Flask(__name__)
 
-@app.route("/webhook",methods=["POST"])
+#@app.route("/webhook",methods=["POST"])
 def main():
-    req = request.get_json()
-    date = req["queryResult"]["parameters"]["date"]
-    weather = req["queryResult"]["parameters"]["weather"]
+    request_json = request.get_json()
+    date = request_json["queryResult"]["parameters"]["date"]
+    weather = request_json["queryResult"]["parameters"]["weather"]
     url = "https://tenki.jp/forecast/7/37/6710/34100/"
 
     date = date.split("T")[0]
@@ -26,21 +26,24 @@ def main():
 
 
     response = req.get(url)
-    print(response.text)
-    soup = BeautifulSoup(response.text,"lxml")
+    soup = BeautifulSoup(response.text,"html.parser")
 
-    if date == datetime.strftime(today,"%Y-%m-%d"):
-       today = soup.find(calss_="today-weather")
-       forecast_weather = today.p.string
+    if date == datetime.datetime.strftime(today,"%Y-%m-%d"):
+       today_html = soup.find(class_="today-weather")
+       forecast_weather = today_html.p.string
+       if forecast_weather == "晴":
+           forecast_weather = "晴れ"
        speech = "今日の広島の天気は{}です".format(forecast_weather)
-    elif data == datetime.strftime(tomorrow,"%Y-%m-%d"):
-        tomorrow = soup.find(class_="tomorrow-weather")
-        forcast_weather = tomorrow.p.string
+    elif data == datetime.datetime.strftime(tomorrow,"%Y-%m-%d"):
+        tomorrow_html = soup.find(class_="tomorrow-weather")
+        forcast_weather = tomorrow_html.p.string
+        if forecast_weather == "晴":
+           forecast_weather = "晴れ"
         speech = "明日の広島の天気は{}です".format(forecast_weather)
 
     res = make_response(jsonify({'speech':speech,'displayText':speech}))
     res.headers['Content-type'] = 'application/json'
-    return res
+    return(res)
 
 
 if __name__ == "__main__":
