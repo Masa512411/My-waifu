e16a357e011ad9cab6bb355b52523db6e34cf47e
Masa512411 <masa1717@gol.com>
2018-12-09 13:17:56 +0900

    [fix]main.py

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ def main():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    #port = int(os.environ.get("PORT",5000))
     
-    app.run(debug=False,port=port,host="0.0.0.0")
+    #app.run(debug=False,port=port,host="0.0.0.0")
+    app.run()
